fe2f3f8672f601e4b0c8d5f0e026de92e4ff39ab
Divya Agrawal <agarwaldivya3101@gmail.com>
2024-01-19 19:25:21 +0530

    Initial commit

---
+++ src/main/java/com/divyaishu/snapship/SnapshipApplication.java
@@ -0,0 +1,13 @@
+package com.divyaishu.snapship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divyaishu/snapship/SnapshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyaishu.snapship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
